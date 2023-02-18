f4997f3c3505f7e981aad07e83fb8cbc4eba6238
Tomeee <diether.noche@gmail.com>
2023-02-18 21:48:06 +0800

    Add some styles to the 404 page

--- src/404/index.jsx
+++ src/404/index.jsx
@@ -1,32 +1,17 @@
 import { Link } from "react-router-dom";
+import "./404.css";
 
 function NotFound() {
   return (
     <>
-      <main>
-        <div className="container vertical-center">
-          <div className="row justify-content-center">
-            <div className="col-lg-6 text-center mb-5">//image here</div>
-            <div className="col-lg-6 text-center text-lg-start">
-              <h1 className="display-1 fw-bold">404</h1>
-              <h2 className="display-3 lead">It seems that You're lost.</h2>
-              <p>
-                The page you are looking for does not exist. How you got here is
-                a mystery. But you can click the button below to go back to the
-                homepage.
-              </p>
-              <Link to="/" className="btn btn--home">
-                HOME
-              </Link>
-              <p className="pt-4">
-                Or you cant contact&nbsp;
-                <Link to="/Contact-us" className="link link--light">
-                  Support
-                </Link>
-                &nbsp;and report the problem.
-              </p>
-            </div>
-          </div>
+      <main className="error-page">
+        <div className="content text-center">
+          <h1 className="error-code">404</h1>
+          <h2 className="error-text">Page not found</h2>
+          <p className="error-text">It seems that you are lost.</p>
+          <Link to="/" className="btn btn-home">
+            Back to Home
+          </Link>
         </div>
       </main>
     </>
