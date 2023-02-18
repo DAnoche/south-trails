65138c3e81c4dd3374b3ec214e3931eef98f9654
Tomeee <diether.noche@gmail.com>
2023-02-19 00:34:51 +0800

    Install BS5 lightbox as a dependency

--- src/Gallery/index.jsx
+++ src/Gallery/index.jsx
@@ -1,7 +1,54 @@
+import Lightbox from "bs5-lightbox";
+
+const options = {
+  keyboard: true,
+  size: "fullscreen",
+};
+
+document.querySelectorAll(".my-lightbox-toggle").forEach((el) =>
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    const lightbox = new Lightbox(el, options);
+    lightbox.show();
+  })
+);
+
 function Gallery() {
   return (
     <>
-      <h1>Hi im Gallery</h1>
+      <div>
+        <a
+          href="https://www.youtube.com/watch?v=k6mFF3VmVAs"
+          data-toggle="lightbox"
+          data-gallery="mixedgallery"
+          class="col-sm-4"
+        >
+          <img
+            src="https://i1.ytimg.com/vi/yP11r5n5RNg/mqdefault.jpg"
+            class="img-fluid"
+          />
+        </a>
+        <a
+          href="https://unsplash.it/1200/768.jpg?image=257"
+          data-toggle="lightbox"
+          data-gallery="mixedgallery"
+          class="col-sm-4"
+        >
+          <img src="https://unsplash.it/600.jpg?image=257" class="img-fluid" />
+        </a>
+        <a
+          href="https://vimeo.com/80629469"
+          data-remote="https://player.vimeo.com/video/80629469"
+          data-toggle="lightbox"
+          data-gallery="mixedgallery"
+          class="col-sm-4"
+        >
+          <img
+            src="https://i.vimeocdn.com/video/80629469_640.png"
+            class="img-fluid"
+          />
+        </a>
+      </div>
     </>
   );
 }
