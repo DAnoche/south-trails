b0e5800dfb343297655a5ab93fa0cd8ccec72231
Tomeee <diether.noche@gmail.com>
2023-02-20 18:49:26 +0800

    Ready for deployment

--- src/Utils/const.js
+++
@@ -1,6 +0,0 @@
-export const myExpressURL = "https://good-erin-pike-suit.cyclic.app";
-
-export const serverRoutes = {
-  Login: myExpressURL + "/api/v2/login",
-  Register: myExpressURL + "/api/v2/register",
-};
